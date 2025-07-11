28251e1b2a9260b412d2771ec76ff2723e6e31ce
Hardleit <al.leiton@duocuc.cl>
2025-07-10 20:26:00 -0400

    Add files via upload

---
+++ integrantes.py
@@ -0,0 +1,12 @@
+# lista_integrantes.py
+
+integrantes = [
+    "Marco Salazar",
+    "Javier Silva",
+    "Jean Balladares",
+    "Alejandro Leiton"
+]
+
+print("Integrantes del grupo:")
+for persona in integrantes:
+    print(f"- {persona}")
